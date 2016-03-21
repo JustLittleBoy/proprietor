80b65d6e2ed3f88380b8e6ec9f2c587f095ba80c
JustLittleBoy <wangchaoxiaoban@gmail.com>
2016-03-21 15:32:11 +0800

    完善路由机制，添加连接带参支持

--- proprietor/Proprietor.php
+++ proprietor/Proprietor.php
@@ -9,8 +9,8 @@
  */
 class Proprietor
 {
-	
-	public static $_autoload=array();
+
+	public static $_autoload = array();
 
 	public static function Init()
 	{
@@ -22,26 +22,38 @@ class Proprietor
 		define('MODELS_PATH', APP_PATH . '/models/'); // 模型路径
 		define('LIBRARL_PATH', APP_PATH . '/library/'); // 模型路径
 		define('CACHE_PATH', APP_PATH . '/cache/'); // 缓存路径
-		define('VIEWPOSTFIX','.html');
-		include_once __DIR__.'/Pdo.php';
-		include_once __DIR__.'/Controller.php';
-		include_once __DIR__.'/ProprietorTool.php';
-		include_once __DIR__.'/ProprietorView.php';
-		include_once __DIR__.'/ProprietorModels.php';
-		//自动注册类
-		spl_autoload_register(array('Proprietor','classAutoLoader'));
+		define('VIEWPOSTFIX', '.html');
+		include_once __DIR__ . '/Pdo.php';
+		include_once __DIR__ . '/Controller.php';
+		include_once __DIR__ . '/ProprietorTool.php';
+		include_once __DIR__ . '/ProprietorView.php';
+		include_once __DIR__ . '/ProprietorModels.php';
+		// 自动注册类
+		spl_autoload_register(array(
+			'Proprietor',
+			'classAutoLoader'
+		));
 		
-		//注册错误屏蔽
-		if(!DEBUG){
-			set_exception_handler(array('Proprietor','exceptionHandler'));
-			set_error_handler(array('Proprietor','errorHandler'),E_ALL);
+		// 注册错误屏蔽
+		if (! DEBUG) {
+			set_exception_handler(array(
+				'Proprietor',
+				'exceptionHandler'
+			));
+			set_error_handler(array(
+				'Proprietor',
+				'errorHandler'
+			), E_ALL);
 		}
 		
-		register_shutdown_function(array('Proprietor','shutdownCatchError'));
+		register_shutdown_function(array(
+			'Proprietor',
+			'shutdownCatchError'
+		));
 		
 		self::autoroutes(); // 路由
 	}
-	
+
 	/**
 	 * 自动路由
 	 */
@@ -54,49 +66,48 @@ class Proprietor
 		 * (2).com/a/f/param1_name/param1_val...
 		 * (3).com/a/f.html(.php)?v_n=v_v&v_n2=v_v2....
 		 */
-	    $http_host=$_SERVER['HTTP_HOST'];
-	    //添加本地访问URL解析支持
-	    $sub_request_str='';
-	    if($http_host== 'localhost' || $http_host='127.0.0.1'){
-	       $url_in=realpath('.');
-	       
-	       $url_in_arr=explode(DS, $url_in);
-	       
-	       if($url_in_arr && !strpos( $url_in_arr[count($url_in_arr)-1],':')){
-	          $sub_request_str=array_pop($url_in_arr);
-	       }
-	    }
-	    
-	    $url = $_SERVER['REQUEST_URI'];
-	 
-	    if($sub_request_str){
-	        $url=substr($url,strpos($url,$sub_request_str)+strlen($sub_request_str));
-	    }
-
+		$http_host = $_SERVER['HTTP_HOST'];
+		// 添加本地访问URL解析支持
+		$sub_request_str = '';
+		if ($http_host == 'localhost' || $http_host == '127.0.0.1') {
+			
+			$url_in = realpath('.');
+			
+			$url_in_arr = explode(DS, $url_in);
+			
+			if ($url_in_arr && ! strpos($url_in_arr[count($url_in_arr) - 1], ':')) {
+				$sub_request_str = array_pop($url_in_arr);
+			}
+		}
+		
+		$url = $_SERVER['REQUEST_URI'];
+		
+		if ($sub_request_str) {
+			$url = substr($url, strpos($url, $sub_request_str) + strlen($sub_request_str));
+		}
+		
 		if ($url{0} == '/') {
 			$url = substr($url, 1);
 		}
 		
-		if(substr($url,-1,1)=='/'){
-			$url=substr($url,0,strlen($url)-1);
+		if (substr($url, - 1, 1) == '/') {
+			$url = substr($url, 0, strlen($url) - 1);
 		}
 		
 		$url_array = explode('/', $url);
 		
 		$count_url_para = count($url_array);
+		
 		$controller = 'Index';
-		$action = 'indexAction';
-		$param = '';
+		$action = 'index';
+		$param = array();
 		if ($count_url_para == 1) {
 			// .com/a.html(.php)
 			$pre_result = preg_match('/^([A-Za-z0-9]+)(\.php|\.html)*/', $url_array[0], $html_url);
 			if ($pre_result) {
-				
 				$controller = strtolower($html_url[1]);
 				$controller = ucfirst($controller);
-				
-				$param = $url;
-				
+				$param = $_GET;
 			}
 		} elseif ($count_url_para == 2) {
 			// (3).com/a/f.html(.php)?v_n=v_v&v_n2=v_v2....
@@ -109,8 +120,8 @@ class Proprietor
 				$pre_result2 = preg_match('/^([A-Za-z0-9]+)(\.php|\.html)*/', $url_array[1], $html_url2);
 				// 方法
 				if ($pre_result2) {
-					$action = $html_url2[1].'Action';
-					$param = $url_array[1];
+					$action = $html_url2[1];
+					$param = $_GET;
 				} else {
 					\ProprietorTool::showSysErrorPage('链接有误，方法名不存在');
 				}
@@ -125,18 +136,18 @@ class Proprietor
 				$controller = strtolower($html_url1[1]);
 				$controller = ucfirst($controller);
 				
-				$pre_result2 = preg_match('/^([A-Za-z0-9]+)(\.php|\.html)*/', $url_array[1], $html_url2);
+				$pre_result2 = preg_match('/^([A-Za-z0-9]+)(\.php|\.html)*([\S\s])?/', $url_array[1], $html_url2);
 				// 方法
 				if ($pre_result2) {
-					$action = $html_url2[1].'Action';
-					$param = $url_array[1];
+					$action = $html_url2[1];
 				} else {
 					\ProprietorTool::showSysErrorPage('链接有误，方法名不存在');
 				}
 			} else {
 				\ProprietorTool::showSysErrorPage('链接有误，控制器不存在');
 			}
-			$param = substr($url, (strpos($url, $html_url2[0]) + strlen($html_url2[0])));
+			$param = substr($url, (strpos($url, $html_url2[0]) + strlen($html_url2[0]) + 1));
+			$param = explode('/', $param);
 		}
 		
 		// 自动加载类
@@ -144,61 +155,61 @@ class Proprietor
 		// 获取当前的控制器和方法
 		define('NOWCLASS', $controller);
 		define('CLASSCONTROLLER', $controller . 'Controller');
-		define('ACTION', $action);
+		define('ACTION', $action . 'Action');
 		
 		$incude_php = CONTROLLER_PATH . CLASSCONTROLLER . '.php';
 		
 		include $incude_php;
 		
-		if(defined('NAMESAPCE')){
-				
-			$namesapce_con=NAMESAPCE.'\\Controller\\'.CLASSCONTROLLER;
-				
-			$class = new $namesapce_con;
-				
-		}else{
-				
-			$class = new CLASSCONTROLLER;
+		if (defined('NAMESAPCE')) {
 			
+			$namesapce_con = NAMESAPCE . '\\Controller\\' . CLASSCONTROLLER;
+			
+			$class = new $namesapce_con();
+		} else {
+			
+			$class = new CLASSCONTROLLER();
 		}
-		//添加参数支持
-		call_user_func(array($class,ACTION));
-	
+		// 添加参数支持
+		@call_user_func(array(
+			$class,
+			ACTION
+		), $param) or die('请求链接有误，请确认方法名[' . $action . ']或控制器[' . $controller . ']存在');
 	}
 
 	/**
 	 * 自动加载类，将文件以键值对方式存储
+	 * 
 	 * @todo 加载进来的类需要存在数组中
 	 */
-	public static function autoload($path='')
+	public static function autoload($path = '')
 	{
-	
+		
 		// 引入控制器
 		if (is_dir(CONTROLLER_PATH)) {
 			$dir_p = opendir(CONTROLLER_PATH);
 			while (($file = readdir($dir_p)) !== false) {
 				if ($file != '.' && $file != '..') {
-					$con_pathinfo=pathinfo(CONTROLLER_PATH . $file);
-					if(defined('NAMESAPCE')){
-						self::$_autoload[NAMESAPCE][$con_pathinfo['filename']]=CONTROLLER_PATH . $file;
-					}else{
-						self::$_autoload[$con_pathinfo['filename']]=CONTROLLER_PATH . $file;
+					$con_pathinfo = pathinfo(CONTROLLER_PATH . $file);
+					if (defined('NAMESAPCE')) {
+						self::$_autoload[NAMESAPCE][$con_pathinfo['filename']] = CONTROLLER_PATH . $file;
+					} else {
+						self::$_autoload[$con_pathinfo['filename']] = CONTROLLER_PATH . $file;
 					}
-	
 				}
 			}
 		}
-	
+		
 		// 加载模型
 		if (is_dir(MODELS_PATH)) {
 			$dir_p = opendir(MODELS_PATH);
 			while (($file = readdir($dir_p)) !== false) {
 				if ($file != '.' && $file != '..') {
-					$con_pathinfo=pathinfo(MODELS_PATH . $file);
-					if(defined('NAMESAPCE')){
-						self::$_autoload[NAMESAPCE][$con_pathinfo['filename']]=MODELS_PATH . $file;
-					}else{
-						self::$_autoload[$con_pathinfo['filename']]=MODELS_PATH . $file;
+					$con_pathinfo = pathinfo(MODELS_PATH . $file);
+					if (defined('NAMESAPCE')) {
+						self::$_autoload[NAMESAPCE][$con_pathinfo['filename']] = MODELS_PATH . $file;
+					} else {
+						self::$_autoload[$con_pathinfo['filename']] = MODELS_PATH . $file;
 					}
 				}
 			}
@@ -209,99 +220,113 @@ class Proprietor
 			$dir_p = opendir(LIBRARL_PATH);
 			while (($file = readdir($dir_p)) !== false) {
 				if ($file != '.' && $file != '..') {
-					$con_pathinfo=pathinfo(LIBRARL_PATH . $file);
-					if(defined('NAMESAPCE')){
-						self::$_autoload[NAMESAPCE][$con_pathinfo['filename']]=LIBRARL_PATH . $file;
-					}else{
-						self::$_autoload[$con_pathinfo['filename']]=LIBRARL_PATH . $file;
+					$con_pathinfo = pathinfo(LIBRARL_PATH . $file);
+					if (defined('NAMESAPCE')) {
+						self::$_autoload[NAMESAPCE][$con_pathinfo['filename']] = LIBRARL_PATH . $file;
+					} else {
+						self::$_autoload[$con_pathinfo['filename']] = LIBRARL_PATH . $file;
 					}
 				}
 			}
 		}
-		
 	}
-	
+
 	/**
 	 * 自动加载类
-	 * @param unknown $name
+	 * 
+	 * @param unknown $name        	
 	 */
-	public static function classAutoLoader($name){
-		if(!self::$_autoload){
+	public static function classAutoLoader($name)
+	{
+		if (! self::$_autoload) {
 			self::autoload();
 		}
 		
-		$name_arr=explode("\\",$name);
-		$name=array_pop($name_arr);
+		$name_arr = explode("\\", $name);
+		$name = array_pop($name_arr);
 		
-		//自动加载控制器
-	//var_dump(self::$_autoload);die;
-		if(defined('NAMESAPCE')){
-	
-			if(!self::$_autoload[NAMESAPCE][$name]){
-				exit;
+		// 自动加载控制器
+		// var_dump(self::$_autoload);die;
+		if (defined('NAMESAPCE')) {
+			
+			if (! self::$_autoload[NAMESAPCE][$name]) {
+				exit();
 			}
-	
+			
 			include self::$_autoload[NAMESAPCE][$name];
-	
-		}else{
-			if(!self::$_autoload[$name]){
-				exit;
+		} else {
+			if (! self::$_autoload[$name]) {
+				exit();
 			}
-	
+			
 			include self::$_autoload[$name];
-	
 		}
 	}
-	
+
 	/**
 	 * 记录警告信息
-	 * @param Exception $e
-	 */	
-	 public static function exceptionHandler(Exception $e){
-		self::log($e->getMessage(), $e->getFile(), $e->getLine(),$e->getCode(), 'exception');
+	 * 
+	 * @param Exception $e        	
+	 */
+	public static function exceptionHandler(Exception $e)
+	{
+		self::log($e->getMessage(), $e->getFile(), $e->getLine(), $e->getCode(), 'exception');
 		self::showMsg(array(
-			'msg'  => $e->getMessage(),
+			'msg' => $e->getMessage(),
 			'file' => $e->getFile(),
 			'line' => $e->getLine(),
-			'code' => $e->getCode(),
+			'code' => $e->getCode()
 		), 'exception');
 	}
-	
+
 	/**
 	 * 抓住致命错误
 	 */
-	public static function shutdownCatchError(){
-	
-		$_error=error_get_last();
-	
-		if($_error && in_array($_error['type'],array(1,4,16,64,256,4096,E_ALL))){
-	
-			$error= '致命错误:'.$_error['message'].'</br>';
-	
-			$error.= '文件:'.$_error['file'].'</br>';
-	
-			$error.= '在第'.$_error['line'].'行</br>';
+	public static function shutdownCatchError()
+	{
+		$_error = error_get_last();
+		
+		if ($_error && in_array($_error['type'], array(
+			1,
+			4,
+			16,
+			64,
+			256,
+			4096,
+			E_ALL
+		))) {
+			
+			$error = '致命错误:' . $_error['message'] . '</br>';
+			
+			$error .= '文件:' . $_error['file'] . '</br>';
+			
+			$error .= '在第' . $_error['line'] . '行</br>';
 			
 			self::showSysErrorPage($error);
 		}
-	
-	}
-	
-	public static function errorHandler($errno, $errstr, $errfile, $errline){
-		self::log($errstr, $errfile, $errline,$errno, 'SysError');
-	}
-	
-	public static function log($msg, $errfile='',$errline='',$errno='',$saveLogPrev='SysError'){
-		$msg  = date('Y-m-d H:i:s ').$_SERVER['QUERY_STRING'].'|'.$errfile.'|'.$errline.'|'.$errno.'|'.str_replace(array("\r", "\n"), array('\\r', '\\n'), $msg)."\r\n";
-		
-		$log_path=APP_PATH.'/cache/log';
-		
-		if(!file_exists($log_path)){
-			mkdir($log_path,'0777',true) ;
-		}
-		
-		error_log($msg, 3, $log_path.'/'.$saveLogPrev.'.log');
-	
 	}
 
+	public static function errorHandler($errno, $errstr, $errfile, $errline)
+	{
+		self::log($errstr, $errfile, $errline, $errno, 'SysError');
+	}
+
+	public static function log($msg, $errfile = '', $errline = '', $errno = '', $saveLogPrev = 'SysError')
+	{
+		$msg = date('Y-m-d H:i:s ') . $_SERVER['QUERY_STRING'] . '|' . $errfile . '|' . $errline . '|' . $errno . '|' . str_replace(array(
+			"\r",
+			"\n"
+		), array(
+			'\\r',
+			'\\n'
+		), $msg) . "\r\n";
+		
+		$log_path = APP_PATH . '/cache/log';
+		
+		if (! file_exists($log_path)) {
+			mkdir($log_path, '0777', true);
+		}
+		
+		error_log($msg, 3, $log_path . '/' . $saveLogPrev . '.log');
+	}
 }
